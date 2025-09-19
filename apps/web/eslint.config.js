d498b095878b1d218e79ba53174e2ffb6bc2cdb4
riveur <riveur.cpy@gmail.com>
2025-09-19 09:38:01 +0200

    chore: initial commit (cloned from riveur/tra-stack)

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,13 @@
+import { configApp, RULES_LIST } from '@repo/eslint-config'
+
+export default configApp({
+  name: 'Custom config for Tanstack Router',
+  files: ['**/*.tsx'],
+  ignores: ['**/routeTree.gen.ts'],
+  rules: RULES_LIST,
+  languageOptions: {
+    parserOptions: {
+      tsconfigRootDir: import.meta.dirname,
+    }
+  }
+})
