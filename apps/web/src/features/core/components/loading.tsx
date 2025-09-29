e304a0aa4a928bd4ec5d7cde899ae76fe3ed7bb8
riveur <riveur.cpy@gmail.com>
2025-09-29 12:34:41 +0200

    refactor(web): remove default message on loading component

--- apps/web/src/features/core/components/loading.tsx
+++ apps/web/src/features/core/components/loading.tsx
@@ -1,20 +1,10 @@
 import { Spinner } from '@/features/core/components/ui/spinner'
 import { cn } from '@/lib/utils'
 
-export function Loading({
-  children = <span>Chargement en cours...</span>,
-  className,
-  ...props
-}: React.ComponentProps<'div'>) {
+export function Loading({ children, className, ...props }: React.ComponentProps<'div'>) {
   return (
-    <div
-      className={cn(
-        'flex flex-col items-center gap-4 [&_svg]:size-16 [&_svg]:animate-spin',
-        className
-      )}
-      {...props}
-    >
-      <Spinner variant="ring" />
+    <div className={cn('flex flex-col items-center gap-4 [&_svg]:size-16', className)} {...props}>
+      <Spinner variant="bars" />
       {children}
     </div>
   )
